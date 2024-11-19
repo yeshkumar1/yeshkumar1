f98317c00f0b955f8fcbcd61778e410b19fada84
yeshkumar1 <nikhilkataria679@gmail.com>
2024-11-19 11:01:08 +0500

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -1,57 +1,83 @@
 public class LinkedList {
-    private Node top;
 
-    public static class Node {
+    private Node front;
+    private Node rear;
+
+
+
+    static class Node{
         public int data;
         public Node next;
-
-
-        public Node(int data) {
-            this.data = data;
+        public  Node (int data){
+            this.data=data;
+            this.next=null;
         }
     }
-    public void insertFirst(int data){
-        Node n = new Node(data);
-        n.next = top;
-        top = n;
+    public boolean isEmpty(){
+        return (front==null);
     }
-    public Node deleteFirst(){
-        Node temp = top;
-        top = top.next;
+    public void insertLast(int new_data){
+        Node new_node = new Node(new_data);
+        if (isEmpty())
+            front = new_node;
+        else rear.next=new_node;
+        rear=new_node;
+    }
+    public int deleteFirst(){
+        int temp = front.data;;
+        if(front.next == null)
+            rear=null;
+        front=front.next;
         return temp;
     }
-    public void displayList(){
-        Node current = top;
-        while(current!= null){
+    public void displaylist(){
+        System.out.println("===Displaying Elements of DEQ===");
+        Node current = front;
+        while (current!=null){
             System.out.println(current.data);
-            current = current.next;
+            current=current.next;
         }
-    }
-}
 
-class LinkedListStack{
-    LinkedList li = new LinkedList();
-    public void push(int element){
-        li.insertFirst(element);
-    }
-    public void pop(){
-        li.deleteFirst();
-    }
-    public void displayList(){
-        System.out.println(" ");
-        li.displayList();
     }
 }
-class LLStackClient{
-    public static void main(String[] args) {
-        LinkedListStack st = new LinkedListStack();
-        st.push(50);
-        st.push(70);
-        st.push(190);
-        System.out.println("1. Stack after push operation: ");
-        st.displayList();
-        System.out.println("1. Stack after pop operation: ");
-        st.pop();
-        st.displayList();
+class Queue_2{
+    LinkedList llist;
+
+    public Queue_2() {
+        llist = new LinkedList(); }
+
+    public boolean isEmpty(){
+        return llist.isEmpty();
     }
+    public void enqueue (int new_data){
+        llist.insertLast (new_data);
+    }
+    public int deque(){
+        return llist.deleteFirst();
+    }
+    void printQueue() {
+        llist.displaylist();
+    }
+}
+class QueueClient {
+    public static void main(String[] args) {
+
+        Queue_2 list=new Queue_2();
+
+        list.enqueue (10);
+
+        list.enqueue (20);
+
+        list.enqueue (30);
+
+        list.enqueue (40);
+
+        list.printQueue();
+
+        list.deque();
+
+        list.printQueue();
+
+    }
+
 }
